e6ac41ba38ba9196437d84ae84130609a73c5c5c
Simon Jentzsch <simon.jentzsch@slock.it>
2016-02-02 13:11:47 +0100

    added russian and french language into the new site-structure

--- js/update.js
+++ js/update.js
@@ -4,6 +4,8 @@
 var langs = [
   ['index.html'   ,'img/icon_en.png','english'], 
   ['index_de.html','img/icon_de.png','german'], 
+  ['index_fr.html','img/icon_fr.png','french'], 
+  ['index_ru.html','img/icon_ru.png','russian'], 
   ['index_es.html','img/icon_es.png','spanish'], 
   ['index_zh.html','img/icon_zh.png','chinese'] 
 ];

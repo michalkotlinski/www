81ea6d0128eade63dc3bdb3f090b3fb2083baeb1
Simon Jentzsch <simon.jentzsch@slock.it>
2016-02-17 12:21:07 +0100

    fixed https-links

--- js/update.js
+++ js/update.js
@@ -3,11 +3,11 @@
 
 var langs = [
   ['index.html'   ,'img/icon_en.png','english'], 
-  //['index_de.html','img/icon_de.png','german'], 
-  //['index_fr.html','img/icon_fr.png','french'], 
-  //['index_ru.html','img/icon_ru.png','russian'], 
-  //['index_es.html','img/icon_es.png','spanish'], 
-  //['index_ja.html','img/icon_ja.png','japanese'], 
+  ['index_de.html','img/icon_de.png','german'], 
+  ['index_fr.html','img/icon_fr.png','french'], 
+  ['index_ru.html','img/icon_ru.png','russian'], 
+  ['index_es.html','img/icon_es.png','spanish'], 
+  ['index_ja.html','img/icon_ja.png','japanese'], 
   ['index_zh.html','img/icon_zh.png','chinese'] 
 ];
 
